b9a00a6c206b5f5c283c6b5db1652ab93df51f27
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 21:11:35 +0900

    feat: @EnableWebSecurity 를 붙인 보안 필터를 구현하는 SecurityConfig 클래스 생성

---
+++ src/main/java/com/example/springsecurityauthorization/configuration/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityauthorization.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+
+}
