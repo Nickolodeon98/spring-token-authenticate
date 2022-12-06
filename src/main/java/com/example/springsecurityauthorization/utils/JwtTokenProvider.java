1ef06542df1099af9ce92be7669d34ed13df8257
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-06 23:37:53 +0900

    feat: 토큰 생성하는 메서드가 있는 JwtTokenProvider 에서 토큰에 대해 인증할 때 필요한 메서드들도 구현

--- src/main/java/com/example/springsecurityauthorization/utils/JwtTokenProvider.java
+++ src/main/java/com/example/springsecurityauthorization/utils/JwtTokenProvider.java
@@ -4,14 +4,26 @@ import com.example.springsecurityauthorization.configuration.UserRole;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtTokenProvider {
 
-    public static String createToken(String username, UserRole userRole) {
+    public static String getUserName(String token, String secretKey) {
+        return extractClaims(token, secretKey).getSubject();
+    }
+
+    public static String getUserRole(String token, String secretKey) {
+        return extractClaims(token, secretKey).get("role", String.class);
+    }
+
+    public static String createToken(String username, UserRole userRole, String secretKey) {
         Claims claims = Jwts.claims().setSubject(username);
         claims.put("role", userRole.name());
         Date now = new Date();
@@ -20,7 +32,20 @@ public class JwtTokenProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + (1000L * 60 * 60)))
-                .signWith(SignatureAlgorithm.HS256, "secretKey")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
+    }
+
+    public static Claims extractClaims(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+
+    public static boolean isExpired(String token, String secretKey) {
+        return extractClaims(token, secretKey).getExpiration().before(new Date());
+    }
+
+    public static Authentication getAuthentication(String userName, String userRole) {
+        // 여기서 setDetails 를 왜 해야하는지?
+        return new UsernamePasswordAuthenticationToken(userName, null, List.of(new SimpleGrantedAuthority(userRole)));
     }
 }
